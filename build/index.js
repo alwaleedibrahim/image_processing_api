ccd9da63f211aaaecba1cb0b1cbec9a5558ffea9
alwaleedibrahim <alwaleed.ibrahim.m@gmail.com>
2021-12-24 15:20:28 +0200

    Added get routes

--- build/index.js
+++ build/index.js
@@ -9,3 +9,12 @@ const app = (0, express_1.default)();
 app.listen(PORT, () => {
     console.log(`Server is listning to port: ${PORT}`);
 });
+app.get("/", (req, res) => {
+    res.send("Please upload an image!");
+});
+app.get("/image", (req, res) => {
+    const imageId = req.query.image;
+    const imageHeight = req.query.height;
+    const imageWidth = req.query.width;
+    res.send("" + imageId + imageHeight + imageWidth);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
-import express, {Application} from 'express';
+import express, {Application, Request, Response} from 'express';
 
-const PORT = 8080;
+const PORT :Number = 8080;
 
 const app :Application = express();
 
 app.listen(PORT, () => {
     console.log(`Server is listning to port: ${PORT}`)
-})
+});
+
+app.get("/", (req :Request, res :Response) => {
+    res.send("Please upload an image!");
+});
+
+app.get("/image", (req :Request, res :Response) => {
+    const imageId = req.query.image;
+    const imageHeight = req.query.height;
+    const imageWidth = req.query.width;
+    res.send("" + imageId + imageHeight + imageWidth);
+});
