2e3383886282aad9a36ef9c006a4dbc8057e6039
alwaleedibrahim <alwaleed.ibrahim.m@gmail.com>
2021-12-28 11:15:07 +0200

    Added tests for image_resizer.ts

--- src/tests/modules/image_resizerSpec.ts
+++ src/tests/modules/image_resizerSpec.ts
@@ -1,3 +1,24 @@
-import supertest from "supertest"
-
 import resizeImage from "../../modules/image_resizer"
+
+describe("Testing resizeImage function", () => {
+    it("Returns a string", async () => {
+        const returnValue = await resizeImage("santamonica", 100, 200);
+        expect(typeof returnValue).toBe("string")
+    })
+    it("Returns error when passing nonexisting image name", async () => {
+        const returnValue = await resizeImage("foo", 100, 200);
+        expect(returnValue).toBe("Error")
+    })
+    it("Returns an error if imageHight is NaN", async () => {
+        const returnValue = await resizeImage("santamonica", NaN, 200);
+        expect(returnValue).toBe("Error")
+    })
+    it("Returns an error if imageWidth is NaN", async () => {
+        const returnValue = await resizeImage("santamonica", 200, NaN);
+        expect(returnValue).toBe("Error")
+    })
+    it("Returns resized image path when succeeds", async () => {
+        const returnValue = await resizeImage("santamonica", 300, 200);
+        expect(returnValue).toBe("assets/resized/santamonica_300_200.jpg")
+    })
+})
